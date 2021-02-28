87a213fe4ca6ff2a1d0fa2a7c833f897490ff18d
dragokl89 <c-adonis@hotmail.com>
2021-02-27 21:58:34 -0500

    final

--- src/main/java/com/devpredator/practicawebservicesconsumer/client/RestaurantesWSClient.java
+++ src/main/java/com/devpredator/practicawebservicesconsumer/client/RestaurantesWSClient.java
@@ -3,10 +3,67 @@
  */
 package com.devpredator.practicawebservicesconsumer.client;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.Invocation;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.GenericType;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import com.devpredator.practicawebservicesconsumer.dto.RestauranteDTO;
+
 /**
  * @author 4PF28LA_2004
  *
  */
 public class RestaurantesWSClient {
+	public static void main(String[] args) {
+		 
+//		Client client = ClientBuilder.newClient();
+//		WebTarget webTarget = client.target("http://localhost:8080/practica-webservices/devpredator/restaurantesWS").path("consultarRestaurantes");
+//		Invocation.Builder invocationBuilder = webTarget.request(MediaType.APPLICATION_JSON);
+//		Response response = invocationBuilder.get();
+//		
+//
+//		
+//	
+//			List<RestauranteDTO> restaurantes =  response.readEntity(new GenericType<List<RestauranteDTO>>() {});
+//			
+//			for (RestauranteDTO restauranteDTO : restaurantes) {
+//				System.out.println(restauranteDTO.getNombre());
+//			}
+//		
+			
+		
+		
+		
+		Client client = ClientBuilder.newClient();
+		WebTarget webTarget = client.target("http://localhost:8080/practica-webservices/devpredator/restaurantesWS")
+				.path("guardarRestaurante");
 
+		RestauranteDTO restaurante = new RestauranteDTO();
+		restaurante.setNombre("Encebollados");
+		restaurante.setIdRestaurante(789L);
+		restaurante.setSlogan("Delicia del mar");
+		restaurante.setDireccion("Carcelen");
+
+		Invocation.Builder invocationBuilder = webTarget.request(MediaType.APPLICATION_JSON);
+		Response response = invocationBuilder.post(Entity.entity(restaurante, MediaType.APPLICATION_JSON));
+
+		if (response.getStatus() == 400) {
+			String error = response.readEntity(String.class);
+			System.out.println(error);
+		}
+
+		if (response.getStatus() == 200) {
+			RestauranteDTO restauranteDTO = response.readEntity(RestauranteDTO.class);
+			System.out.println("Nombre del Restaurante :" + restauranteDTO.getNombre());
+
+		}
+	}
 }
--- src/main/java/com/devpredator/practicawebservicesconsumer/dto/RestauranteDTO.java
+++ src/main/java/com/devpredator/practicawebservicesconsumer/dto/RestauranteDTO.java
@@ -1,9 +1,62 @@
 package com.devpredator.practicawebservicesconsumer.dto;
+
 /**
  * 
  * @author 4PF28LA_2004
  *
  */
 public class RestauranteDTO {
+	private Long idRestaurante;
+	private String nombre;
+	private String direccion;
+	private String slogan;
+	/**
+	 * @return the idRestaurante
+	 */
+	public Long getIdRestaurante() {
+		return idRestaurante;
+	}
+	/**
+	 * @param idRestaurante the idRestaurante to set
+	 */
+	public void setIdRestaurante(Long idRestaurante) {
+		this.idRestaurante = idRestaurante;
+	}
+	/**
+	 * @return the nombre
+	 */
+	public String getNombre() {
+		return nombre;
+	}
+	/**
+	 * @param nombre the nombre to set
+	 */
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	/**
+	 * @return the direccion
+	 */
+	public String getDireccion() {
+		return direccion;
+	}
+	/**
+	 * @param direccion the direccion to set
+	 */
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+	/**
+	 * @return the slogan
+	 */
+	public String getSlogan() {
+		return slogan;
+	}
+	/**
+	 * @param slogan the slogan to set
+	 */
+	public void setSlogan(String slogan) {
+		this.slogan = slogan;
+	}
 
 }
