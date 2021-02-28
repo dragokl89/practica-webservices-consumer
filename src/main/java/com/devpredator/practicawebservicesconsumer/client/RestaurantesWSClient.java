f25be4e1c3aeffc4d905c2ed02c91f56f07b81e6
dragokl89 <c-adonis@hotmail.com>
2021-02-27 19:10:24 -0500

    Inicio del consumidor de la practica

---
+++ src/main/java/com/devpredator/practicawebservicesconsumer/client/RestaurantesWSClient.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package com.devpredator.practicawebservicesconsumer.client;
+
+/**
+ * @author 4PF28LA_2004
+ *
+ */
+public class RestaurantesWSClient {
+
+}
---
+++ src/main/java/com/devpredator/practicawebservicesconsumer/dto/RestauranteDTO.java
@@ -0,0 +1,9 @@
+package com.devpredator.practicawebservicesconsumer.dto;
+/**
+ * 
+ * @author 4PF28LA_2004
+ *
+ */
+public class RestauranteDTO {
+
+}
